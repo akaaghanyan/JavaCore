7d96de93986e74c9c75fb8e21309d1d5cb44b991
User <akaaghanyan668@gmail.com>
2023-08-26 13:13:33 +0400

    classis

---
+++ src/classwork/chepter2/classis.java
@@ -0,0 +1,14 @@
+package classwork.chepter2;
+
+public class classis {public static void main(String[] args){
+      int[] numbers = new int[5];
+      numbers[0] = 55;
+      numbers[1] = 44;
+      numbers[2] = 33;
+      numbers[3] = 22;
+      numbers[4] = 11;
+      System.out.println(numbers[4]);
+}
+
+
+}
